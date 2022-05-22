f7d3010571a424a45f9aa0d6f1039b45f1b2205b
danenders <danenders@gmail.com>
2022-05-22 10:21:30 -0400

    MegaRing Event added

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1217,10 +1217,10 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
+#define FLAG_RECEIVED_MEGA_RING                                    	0x493 // Received from Steven in Mossdeep
+#define FLAG_FOUND_SCEPTILITE                                      	0x494 // Received from Steven in Mossdeep if starter, otherwise found in
+#define FLAG_FOUND_BLAZIKENITE                                     	0x495 // Received from Steven in Mossdeep if starter, otherwise found in
+#define FLAG_FOUND_SWAMPERTITE                                      0x496 // Received from Steven in Mossdeep if starter, otherwise found in
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
@@ -1258,6 +1258,7 @@
 #define FLAG_UNUSED_0x4B9                                           0x4B9 // Unused Flag
 #define FLAG_UNUSED_0x4BA                                           0x4BA // Unused Flag
 #define FLAG_UNUSED_0x4BB                                           0x4BB // Unused Flag
+
 #define FLAG_UNUSED_0x4BC                                           0x4BC // Unused Flag
 #define FLAG_UNUSED_0x4BD                                           0x4BD // Unused Flag
 #define FLAG_UNUSED_0x4BE                                           0x4BE // Unused Flag
